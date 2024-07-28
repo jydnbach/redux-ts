9345884b41f8a9d23bdb66e7522f56357ab73d25
jydnbach <jaydenkim95@gmail.com>
2024-07-28 10:41:11 +0900

    reducer setup

---
+++ src/state/reducers/repositoriesReducer.tsx
@@ -0,0 +1,20 @@
+interface RepositoriesState {
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+const reducer = (state: RepositoriesState, action: any) => {
+  switch (action.type) {
+    case 'search_repositores':
+      return { loading: true, error: null, data: [] };
+    case 'search_repositories_succes':
+      return { loading: false, error: null, data: action.payload };
+    case 'search_repositories_error':
+      return { loading: false, error: action.payload, data: [] };
+    default:
+      return;
+  }
+};
+
+export default reducer;
