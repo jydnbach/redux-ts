254781a79876e6cf2e42042f9f2003ddc94d0408
jydnbach <jaydenkim95@gmail.com>
2024-07-30 13:37:46 +0900

    selecting state

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { useActions } from '../hooks/useActions';
+import { useSelector } from 'react-redux';
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
   const { searchRepositories } = useActions();
+  const { data, error, loading } = useSelector((state) => state.repositories);
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
